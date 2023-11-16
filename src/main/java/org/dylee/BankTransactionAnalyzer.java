e95cea44a067bce10d7dd68d242da767ff502249
lee_dayoung <lee_dayoung@cocone.co.jp>
2023-11-16 21:45:51 +0900

    ADD: 분리된 CSV 클래스 이용하여 리팩터링

---
+++ src/main/java/org/dylee/BankTransactionAnalyzer.java
@@ -0,0 +1,45 @@
+package org.dylee;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.time.Month;
+import java.util.ArrayList;
+import java.util.List;
+
+public class BankTransactionAnalyzer {
+    private static final String RESOURCES = "src/main/resources/";
+
+    public static void main(final String... args) throws IOException {
+
+        final BankStatementCSVParser bankStatementCSVParser = new BankStatementCSVParser();
+
+        final String fileName = args[0];
+        final Path path = Paths.get(RESOURCES + fileName);
+        final List<String> lines = Files.readAllLines(path); // 행 목록 가져오기
+
+        final List<BankTransaction> bankTransactions = bankStatementCSVParser.parseLinesFromCSV(lines);
+
+        System.out.println("Total for all transaction is " + calculateTotalAmount(bankTransactions));
+        System.out.println("Transactions in January " + selectInMonth(bankTransactions, Month.JANUARY));
+    }
+
+    public static double calculateTotalAmount(final List<BankTransaction> bankTransactions){
+        double total = 0d;
+        for (final BankTransaction bankTransaction: bankTransactions) {
+            total += bankTransaction.getAmount();
+        }
+        return total;
+    }
+
+    public static List<BankTransaction> selectInMonth(final List<BankTransaction> bankTransactions, final Month month) {
+        final List<BankTransaction> bankTransactionsInMonth = new ArrayList<>();
+        for (final BankTransaction bankTransaction: bankTransactions) {
+            if (bankTransaction.getDate().getMonth() == month) {
+                bankTransactionsInMonth.add(bankTransaction);
+            }
+        }
+        return bankTransactionsInMonth;
+    }
+}
