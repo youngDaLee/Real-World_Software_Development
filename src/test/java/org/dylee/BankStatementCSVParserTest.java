63b13596a0c0790554340b757a1325f1866a7094
lee_dayoung <lee_dayoung@cocone.co.jp>
2023-11-16 22:22:37 +0900

    TEST: 테스트 코드 추가

---
+++ src/test/java/org/dylee/BankStatementCSVParserTest.java
@@ -0,0 +1,16 @@
+package org.dylee;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class BankStatementCSVParserTest {
+
+    private final BankStatementParser statementParser = new BankStatementCSVParser();
+
+    @Test
+    private void shouldParseOneCorrectLine() throws Exception {
+        Assertions.fail("Not yet implemented");
+    }
+}
