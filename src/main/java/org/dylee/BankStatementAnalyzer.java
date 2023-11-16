8a68e0590feedb0e715572156ce9a1a7ad01f58d
lee_dayoung <lee_dayoung@cocone.co.jp>
2023-11-16 22:10:47 +0900

    FIX: 결합 제거 응용 프로그램

--- src/main/java/org/dylee/BankStatementAnalyzer.java
+++ src/main/java/org/dylee/BankStatementAnalyzer.java
@@ -5,10 +5,9 @@ import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.time.Month;
-import java.util.ArrayList;
 import java.util.List;
 
-public class BankTransactionAnalyzer {
+public class BankStatementAnalyzer {
     private static final String RESOURCES = "src/main/resources/";
 
     public static void analyze(final String fileName, final BankStatementParser bankStatementParser) throws IOException {
--- src/main/java/org/dylee/Main.java
+++ src/main/java/org/dylee/Main.java
@@ -1,7 +1,13 @@
 package org.dylee;
 
+import java.io.IOException;
+
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+    public static void main(String[] args) throws IOException {
+        final BankStatementAnalyzer bankStatementAnalyzer = new BankStatementAnalyzer();
+
+        final BankStatementParser bankStatementParser = new BankStatementCSVParser();
+
+        BankStatementAnalyzer.analyze(args[0], bankStatementParser);
     }
 }
