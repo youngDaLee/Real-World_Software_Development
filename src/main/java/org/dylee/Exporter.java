853c9c6e2ba9e39c125c4fea11df25e90f45c3de
lee_dayoung <lee_dayoung@cocone.co.jp>
2024-01-03 23:32:24 +0900

    FIX: ch03 - 출력 인터페이스 추가

---
+++ src/main/java/org/dylee/Exporter.java
@@ -0,0 +1,8 @@
+package org.dylee;
+
+/**
+ * 출력 인터페이스
+ */
+public interface Exporter {
+    String export(SummaryStatistics summaryStatistics);
+}
---
+++ src/main/java/org/dylee/HtmlExporter.java
@@ -0,0 +1,20 @@
+package org.dylee;
+
+public class HtmlExporter implements Exporter {
+    @Override
+    public String export(final SummaryStatistics summaryStatistics) {
+        String result = "<!doctype html>";
+        result += "<html lang='en'";
+        result += "<head><title>Bank Transaction Report</title></head>";
+        result += "<body>";
+        result += "<ui>";
+        result += "<li><strong>The sum is</strong>" + summaryStatistics.getSum() + "</li>";
+        result += "<li><strong>The average is</strong>" + summaryStatistics.getAverage() + "</li>";
+        result += "<li><strong>The max is</strong>" + summaryStatistics.getMax() + "</li>";
+        result += "<li><strong>The min is</strong>" + summaryStatistics.getMin() + "</li>";
+        result += "</ui>";
+        result += "</body>";
+        result += "</html>";
+        return result;
+    }
+}
