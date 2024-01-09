6c38a293fc89f8da125f28dd13d401c3ae7ee7ba
lee_dayoung <lee_dayoung@cocone.co.jp>
2024-01-09 22:30:31 +0900

    ADD : 레터, 리포트 임포터 클래스 구현

---
+++ src/main/java/org/dylee/ReportImporter.java
@@ -0,0 +1,21 @@
+package org.dylee;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.Map;
+
+import static org.dylee.Constant.*;
+
+public class ReportImporter implements Importer {
+    private static final String NAME_PREFIX = "Patient: ";
+    @Override
+    public Document importFile(final File file) throws IOException {
+        final TextFile textFile = new TextFile(file);
+        textFile.addLineSuffix(NAME_PREFIX, PATIENT);
+        textFile.addLines(2, line -> false, BODY);
+
+        final Map<String, String> attributes = textFile.getAttributes();
+        attributes.put(TYPE, "REPORT");
+        return new Document(attributes);
+    }
+}
