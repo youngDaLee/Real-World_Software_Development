9ee91222a3b4a21520e052585f12fd11e1b744fb
lee_dayoung <lee_dayoung@cocone.co.jp>
2024-01-09 21:40:48 +0900

    ADD : 상수 정의 클래스

---
+++ src/main/java/org/dylee/Constant.java
@@ -0,0 +1,11 @@
+package org.dylee;
+
+/**
+ * 상수 정의 클래스
+ */
+public class Constant {
+    public static final String PATH = "path";
+    public static final String WIDTH = "width";
+    public static final String HEIGHT = "height";
+    public static final String TYPE = "type";
+}
