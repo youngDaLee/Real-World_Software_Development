eee877034964e3778c6c91ec389a5f3a39475a3d
lee_dayoung <lee_dayoung@cocone.co.jp>
2024-01-09 22:30:16 +0900

    ADD : 도큐먼트 관리 시스템 정의

---
+++ src/main/java/org/dylee/DocumentManagementSystem.java
@@ -0,0 +1,50 @@
+package org.dylee;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+/**
+ * 파일 관리 시스템 클래스
+ */
+public class DocumentManagementSystem {
+    private final List<Document> documents = new ArrayList<>();
+    private final Map<String, Importer> extensionToImporter = new HashMap<>();
+
+    public DocumentManagementSystem() {
+        extensionToImporter.put("letter", new LetterImporter());
+        extensionToImporter.put("report", new ReportImporter());
+        extensionToImporter.put("jpg", new ImageImporter());
+    }
+
+    public void importFile(final String path) throws IOException {
+        final File file = new File(path);
+        if (!file.exists()) {
+            throw new FileNotFoundException(path);
+        }
+
+        final int separatorIndex = path.lastIndexOf('.');
+        if (separatorIndex != -1) {
+            if (separatorIndex == path.length()) {
+//                throw new UnknownFileTypeException("No extension found For file: " + path);
+                throw new FileNotFoundException("No extension found For file: " + path);
+            }
+            final String extension = path.substring(separatorIndex + 1);
+            final Importer importer = extensionToImporter.get(extension);
+            if (importer == null) {
+//                throw new UnknownFileTypeException("For file: " + path);
+                throw new FileNotFoundException("For file: " + path);
+            }
+
+            final Document document = importer.importFile(file);
+            documents.add(document);
+        } else {
+//            throw new UnknownFileTypeException("No extension found For file: " + path);
+            throw new FileNotFoundException("No extension found For file: " + path);
+        }
+    }
+}
