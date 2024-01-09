dba84b7feabcbf61ea9235d49bdbd22ae10d29c0
lee_dayoung <lee_dayoung@cocone.co.jp>
2024-01-09 21:41:04 +0900

    ADD : ImageImporter 구현 클래스 추가

---
+++ src/main/java/org/dylee/ImageImporter.java
@@ -0,0 +1,28 @@
+package org.dylee;
+
+import javax.imageio.ImageIO;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+
+import static org.dylee.Constant.*;
+
+/**
+ * ImageImporter 구현 클래스
+ */
+public class ImageImporter implements Importer {
+    @Override
+    public Document importFile(final File file) throws IOException {
+        final Map<String, String> attributes = new HashMap<>();
+        attributes.put(PATH, file.getPath());
+
+        final BufferedImage image = ImageIO.read(file);
+        attributes.put(WIDTH, String.valueOf(image.getWidth()));
+        attributes.put(HEIGHT, String.valueOf(image.getHeight()));
+        attributes.put(TYPE, "IMAGE");
+
+        return new Document(attributes);
+    }
+}
