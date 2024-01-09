967c6432a3aa574ce4f72afbd03c85793254d55e
lee_dayoung <lee_dayoung@cocone.co.jp>
2024-01-09 22:30:44 +0900

    FIX : 상수 정의

--- src/main/java/org/dylee/Constant.java
+++ src/main/java/org/dylee/Constant.java
@@ -8,4 +8,7 @@ public class Constant {
     public static final String WIDTH = "width";
     public static final String HEIGHT = "height";
     public static final String TYPE = "type";
+    public static final String PATIENT = "patient";
+    public static final String BODY = "body";
+    public static final String ADDRESS = "address";
 }
