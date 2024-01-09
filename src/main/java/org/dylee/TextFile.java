2581ab3c31fc5e5d4d955ffa5cf03678372f4903
lee_dayoung <lee_dayoung@cocone.co.jp>
2024-01-09 22:31:28 +0900

    FIX : TextFile 구현, 여러 Importer에서 공용으로 사용하는 메서드 추가

--- src/main/java/org/dylee/TextFile.java
+++ src/main/java/org/dylee/TextFile.java
@@ -1,7 +1,15 @@
 package org.dylee;
 
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.function.Predicate;
+import java.util.stream.Collectors;
+
+import static org.dylee.Constant.PATH;
 
 /**
  * 텍스트 파일 처리 공동 메서드를 처리하기 위한 도메인 클래스
@@ -10,12 +18,37 @@ public class TextFile {
     private final Map<String, String> attributes;
     private final List<String> lines;
 
-    TextFile(Map<String, String> attributes, List<String> lines) {
-        this.attributes = attributes;
-        this.lines = lines;
+    TextFile(final File file) throws IOException {
+        this.attributes = new HashMap<>();
+        this.attributes.put(PATH, file.getPath());
+        this.lines = Files.lines(file.toPath()).collect(Collectors.toList());
     }
 
-    void addLineSuffinx(final String prefix, final String attributeName) {
+    Map<String, String> getAttributes() {
+        return attributes;
+    }
+
+    int addLines(
+            final int start,
+            final Predicate<String> isEnd,
+            final String attributeName) {
+
+        final StringBuilder accumulator = new StringBuilder();
+        int lineNumber;
+        for (lineNumber = start; lineNumber < lines.size(); lineNumber++) {
+            final String line = lines.get(lineNumber);
+            if (isEnd.test(line)) {
+                break;
+            }
+
+            accumulator.append(line);
+            accumulator.append("\n");
+        }
+        attributes.put(attributeName, accumulator.toString().trim());
+        return lineNumber;
+    }
+
+    void addLineSuffix(final String prefix, final String attributeName) {
         for(final String line: lines) {
             if (line.startsWith(prefix)) {
                 attributes.put(attributeName, line.substring(prefix.length()));
