a9da4143e1fe4a78e8df356ab30d5c6dc03784de
lee_dayoung <lee_dayoung@cocone.co.jp>
2024-01-11 20:48:59 +0900

    Fix : switch문으로 분기 처리

--- src/main/java/org/dylee/Main.java
+++ src/main/java/org/dylee/Main.java
@@ -5,11 +5,32 @@ public class Main {
         var evn = new Facts();
         BusinessRuleEngine businessRuleEngine = new BusinessRuleEngine(evn);
 
+//        businessRuleEngine.addAction(facts -> {
+//            var jobTitle = facts.getFact("jobTitle");
+//            if ("CEO".equals(jobTitle)) {
+//                var name = facts.getFact("name");
+//                Mailer.sendEmail("sales@company.com", "Relevant customer: " + name);
+//            }
+//        });
+
         businessRuleEngine.addAction(facts -> {
-            var jobTitle = facts.getFact("jobTitle");
-            if ("CEO".equals(jobTitle)) {
-                var name = facts.getFact("name");
-                Mailer.sendEmail("sales@company.com", "Relevant customer: " + name);
+            var forecastedAmount = 0.0;
+            var dealStage = Stage.valueOf(facts.getFact("stage"));
+            var amount = Double.parseDouble(facts.getFact("amount"));
+            switch (dealStage) {
+                case LEAD:
+                    forecastedAmount = amount * 0.2;
+                    break;
+                case EVALUATING:
+                    forecastedAmount = amount * 0.5;
+                    break;
+                case INTERESTED:
+                    forecastedAmount = amount * 0.8;
+                    break;
+                case CLOSED:
+                    forecastedAmount = amount;
+                    break;
+
             }
         });
     }
