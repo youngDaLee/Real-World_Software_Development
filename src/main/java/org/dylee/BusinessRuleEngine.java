6ae6700cc8c5a48d94988f0511ca422b8d226838
lee_dayoung <lee_dayoung@cocone.co.jp>
2024-01-11 20:07:17 +0900

    ADD : 테스트코드에 따라 로직 추가

--- src/main/java/org/dylee/BusinessRuleEngine.java
+++ src/main/java/org/dylee/BusinessRuleEngine.java
@@ -1,16 +1,24 @@
 package org.dylee;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * 기본 기능 API
  */
 public class BusinessRuleEngine {
+    private final List<Action> actions;
+
+    public BusinessRuleEngine() {
+        this.actions = new ArrayList<>();
+    }
 
     public void addAction(final Action action) {
-        throw new UnsupportedOperationException();
+        this.actions.add(action);
     }
 
     public int count() {
-        throw new UnsupportedOperationException();
+        return this.actions.size();
     }
 
     public void run() {
