7a1b68e6ec88a5ebdee0ce1c5e7b75db41829c0b
lee_dayoung <lee_dayoung@cocone.co.jp>
2024-01-11 20:51:19 +0900

    Fix : fall-through 방지

--- src/main/java/org/dylee/Main.java
+++ src/main/java/org/dylee/Main.java
@@ -14,24 +14,14 @@ public class Main {
 //        });
 
         businessRuleEngine.addAction(facts -> {
-            var forecastedAmount = 0.0;
             var dealStage = Stage.valueOf(facts.getFact("stage"));
             var amount = Double.parseDouble(facts.getFact("amount"));
-            switch (dealStage) {
-                case LEAD:
-                    forecastedAmount = amount * 0.2;
-                    break;
-                case EVALUATING:
-                    forecastedAmount = amount * 0.5;
-                    break;
-                case INTERESTED:
-                    forecastedAmount = amount * 0.8;
-                    break;
-                case CLOSED:
-                    forecastedAmount = amount;
-                    break;
-
-            }
+            var forecastedAmount = amount * switch (dealStage) {
+                case LEAD -> 0.2;
+                case EVALUATING -> 0.5;
+                case INTERESTED -> 0.8;
+                case CLOSED -> 1;
+            };
         });
     }
 }
