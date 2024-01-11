9a894febdac4c185bc7e6950c1376977299ae948
lee_dayoung <lee_dayoung@cocone.co.jp>
2024-01-11 19:47:42 +0900

    ADD : 기본 기능 API 추가

---
+++ src/main/java/org/dylee/BusinessRuleEngine.java
@@ -0,0 +1,19 @@
+package org.dylee;
+
+/**
+ * 기본 기능 API
+ */
+public class BusinessRuleEngine {
+
+    public void addAction(final Action action) {
+        throw new UnsupportedOperationException();
+    }
+
+    public int count() {
+        throw new UnsupportedOperationException();
+    }
+
+    public void run() {
+        throw new UnsupportedOperationException();
+    }
+}
