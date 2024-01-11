c5de0a03fb0f586a8a78b698a1770206e5d52415
lee_dayoung <lee_dayoung@cocone.co.jp>
2024-01-11 21:00:08 +0900

    Fix : 테스트코드 위치 변경

--- src/main/java/org/dylee/Main.java
+++ src/main/java/org/dylee/Main.java
@@ -2,27 +2,6 @@ package org.dylee;
 
 public class Main {
     public static void main(String[] args) {
-        var evn = new Facts();
-        BusinessRuleEngine businessRuleEngine = new BusinessRuleEngine(evn);
-
-//        businessRuleEngine.addAction(facts -> {
-//            var jobTitle = facts.getFact("jobTitle");
-//            if ("CEO".equals(jobTitle)) {
-//                var name = facts.getFact("name");
-//                Mailer.sendEmail("sales@company.com", "Relevant customer: " + name);
-//            }
-//        });
-
-        businessRuleEngine.addAction(facts -> {
-            var dealStage = Stage.valueOf(facts.getFact("stage"));
-            var amount = Double.parseDouble(facts.getFact("amount"));
-            var forecastedAmount = amount * switch (dealStage) {
-                case LEAD -> 0.2;
-                case EVALUATING -> 0.5;
-                case INTERESTED -> 0.8;
-                case CLOSED -> 1;
-            };
-            facts.addFact("forecastedAmount", String.valueOf(forecastedAmount));
-        });
+        System.out.println("Hello");
     }
 }
--- src/test/java/org/dylee/BusinessRuleEngineTest.java
+++ src/test/java/org/dylee/BusinessRuleEngineTest.java
@@ -19,23 +19,42 @@ class BusinessRuleEngineTest {
 
     @Test
     void shouldAddTwoActions() {
-        final BusinessRuleEngine businessRuleEngine = new BusinessRuleEngine();
+        var evn = new Facts();
+        final BusinessRuleEngine businessRuleEngine = new BusinessRuleEngine(evn);
 
-        businessRuleEngine.addAction(() -> {});
-        businessRuleEngine.addAction(() -> {});
+        businessRuleEngine.addAction(facts -> {
+            var jobTitle = facts.getFact("jobTitle");
+            if ("CEO".equals(jobTitle)) {
+                var name = facts.getFact("name");
+                Mailer.sendEmail("sales@company.com", "Relevant customer: " + name);
+            }
+        });
+
+        businessRuleEngine.addAction(facts -> {
+            var dealStage = Stage.valueOf(facts.getFact("stage"));
+            var amount = Double.parseDouble(facts.getFact("amount"));
+            var forecastedAmount = amount * switch (dealStage) {
+                case LEAD -> 0.2;
+                case EVALUATING -> 0.5;
+                case INTERESTED -> 0.8;
+                case CLOSED -> 1;
+            };
+            facts.addFact("forecastedAmount", String.valueOf(forecastedAmount));
+        });
 
         assertEquals(2, businessRuleEngine.count());
     }
 
     @Test
     void shouldExecuteOneAction() {
-        final BusinessRuleEngine businessRuleEngine = new BusinessRuleEngine();
+        var evn = new Facts();
+        final BusinessRuleEngine businessRuleEngine = new BusinessRuleEngine(evn);
         final Action mockAction = mock(Action.class);
 
         businessRuleEngine.addAction(mockAction);
         businessRuleEngine.run();
 
-        verify(mockAction).execute();
+        verify(mockAction).execute(evn);
     }
 
     @Test
