463219b2971cc4dbdaa3ed278e9136aa16ecd65a
lee_dayoung <lee_dayoung@cocone.co.jp>
2024-01-11 20:31:30 +0900

    ADD : Facts 클래스 생성

--- src/main/java/org/dylee/Facts.java
+++ src/main/java/org/dylee/Facts.java
@@ -1,7 +1,19 @@
 package org.dylee;
 
+import java.util.HashMap;
+import java.util.Map;
+
 /**
  * 비즈니스 규칙 엔진 일부의 필요한 상태
  */
 public class Facts {
+    private final Map<String, String> facts = new HashMap<>();
+
+    public String getFact(final String name) {
+        return this.facts.get(name);
+    }
+
+    public void addFact(final String name, final String value) {
+        this.facts.put(name, value);
+    }
 }
